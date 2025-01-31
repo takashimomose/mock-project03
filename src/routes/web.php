03a76960657df1106e888c81cdfc116a599b9e44
takashimomose <taka.yao100@gmail.com>
2025-01-31 11:26:39 +0900

    会員登録完了画面へのルート追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::group([], function () {
     Route::get('/register', [RegisterController::class, 'show'])->name('register.show');
     Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+    Route::get('/thanks', [RegisterController::class, 'thanks'])->name('register.thanks');
 
     Route::get('/login', [AuthController::class, 'show'])->name('auth.show');
 });
